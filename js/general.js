54b12dec8b579a332ec58b8298c2a0646729e9bb
Julian Niebieskikwiat <juliannieb@gmail.com>
2017-09-12 01:07:08 -0500

    Set canvas height equals to the window's height

--- js/general.js
+++ js/general.js
@@ -1,3 +1,8 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
+    $("#canvas").height($(window).height() - $("#navbar").height());
+
+    $(window).resize(function(){
+        $("#canvas").height($(window).height() - $("#navbar").height());
+    });
 })
