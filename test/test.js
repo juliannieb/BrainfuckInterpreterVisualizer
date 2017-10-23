21587aa93c9bdb409462955fe7bfb45d8b5276b9
Julian Niebieskikwiat <juliannieb@gmail.com>
2017-10-22 20:06:30 -0500

    Add increment and decrement value tests

--- test/test.js
+++ test/test.js
@@ -36,11 +36,41 @@ describe('BrainfuckInterpreter', () => {
             // not equal to 2.
             // This will fail if "interpreter.currentCellIdx" is
             // not equal to 0.
+            // This will fail if if "interpreter.memory[interpreter.currentCellIdx].value" is
+            // not equal to 0.
             interpreter.runCommand('>', 0);
             interpreter.runCommand('<', 0);
             assert.equal(2, interpreter.memory.length);
             assert.equal(0, interpreter.currentCellIdx);
             assert.equal(0, interpreter.memory[interpreter.currentCellIdx].value);
+        });
+
+        it('commandIncrementValue', () => {
+            // This will fail if "interpreter.memory.length" is
+            // not equal to 1.
+            // This will fail if "interpreter.currentCellIdx" is
+            // not equal to 0.
+            // This will fail if if "interpreter.memory[interpreter.currentCellIdx].value" is
+            // not equal to 1.
+            interpreter.runCommand('+', 0);
+            assert.equal(1, interpreter.memory.length);
+            assert.equal(0, interpreter.currentCellIdx);
+            assert.equal(1, interpreter.memory[interpreter.currentCellIdx].value);
+        });
+
+        it('commandDecrementValue', () => {
+            // This will fail if "interpreter.memory.length" is
+            // not equal to 1.
+            // This will fail if "interpreter.currentCellIdx" is
+            // not equal to 0.
+            // This will fail if if "interpreter.memory[interpreter.currentCellIdx].value" is
+            // not equal to 1.
+            interpreter.runCommand('+', 0);
+            interpreter.runCommand('+', 0);
+            interpreter.runCommand('-', 0);
+            assert.equal(1, interpreter.memory.length);
+            assert.equal(0, interpreter.currentCellIdx);
+            assert.equal(1, interpreter.memory[interpreter.currentCellIdx].value);
         });
 
     });
